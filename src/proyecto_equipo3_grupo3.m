a7fa1e08bfe98c383ce1bcd06b424352e3849a51
Brandon <brandon.jimenezcampos@ucr.ac.cr>
2025-10-16 21:46:24 -0600

    Agrego de modelos de planta utilizando systemIdentification, sólo se logró un máximo de 64%

--- src/proyecto_equipo3_grupo3.m
+++ src/proyecto_equipo3_grupo3.m
@@ -2,15 +2,20 @@
 data = readmatrix("../Data/150_120_CQRobot.csv");
 
 t = data(:,1); %tiempo
-u = data(:,2); %señal de control
+r = data(:,2); %señal de referencia
 y = data(:,3); %señal de salida
 
 
 figure(1);
-plot(t,u,t,y,'LineWidth',1.5);
+plot(t,r,t,y,'LineWidth',1.5);
 xlabel('Tiempo (s)','FontSize',14);
 ylabel('Amplitud de la señal','FontSize',14);
-legend('u(t)', 'y(t)','FontSize',14);
+legend('r(t)', 'y(t)','FontSize',14);
 title('Señal de control y salida del motor CQ Robot cambio de 150 a 120', 'FontSize',18);
 grid on;
+
+systemIdentification();
+
+
+
 
