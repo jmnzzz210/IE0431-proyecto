19fd9fc41cf8d1034e053b5d72d7ea727c5638be
Brandon <brandon.jimenezcampos@ucr.ac.cr>
2025-10-20 20:08:05 -0600

    Arreglo de gráfica sin DC offset

--- src/proyecto_equipo3_grupo3.m
+++ src/proyecto_equipo3_grupo3.m
@@ -2,34 +2,39 @@
 data = readmatrix("../Data/150_120_CQRobot.csv");
 
 t = data(:,1); %tiempo
-r = data(:,2); %señal de referencia
+u = data(:,2); %señal de referencia
 y = data(:,3); %señal de salida
 
 %% Gráfica de la salida del sistema
 figure(1);
-plot(t,r,t,y,'LineWidth',1.5);
+plot(t,u,t,y,'LineWidth',1.5);
 xlabel('Tiempo (s)','FontSize',14);
 ylabel('Amplitud de la señal','FontSize',14);
-legend('r(t)', 'y(t)','FontSize',14);
+legend('u(t)', 'y(t)','FontSize',14);
 title('Señal de control y salida del motor CQ Robot cambio de 150 a 120', 'FontSize',18);
 grid on;
 
-%% Eliminar el "dc offset"
-r_sindc = r - mean(r);
-y_sindc = y - mean(y);
+%% Eliminar el "dc offset
+muestras_iniciales = min(100, length(t));
+dc_offset_u = mean(u(1:muestras_iniciales));
+dc_offset_y = mean(y(1:muestras_iniciales));
+
+u_sindc = u - dc_offset_u - 10;
+y_sindc = y - dc_offset_y - 5;
+
 Ts = t(2) - t(1);
 
 %% Gráfica de la salida del sistema sin DC offset
 figure(2);
-plot(t, r_sindc, t, y_sindc, 'LineWidth',1.5);
+plot(t, u_sindc, t, y_sindc, 'LineWidth',1.5);
 xlabel('Tiempo (s)', 'FontSize',14);
 ylabel ('Amplitud de la señal', 'FontSize',14);
-legend ('r(t)', 'y(t)', 'FontSize',14);
-legend ('r(t)', 'y(t)', 'FontSize',14);
+legend ('u(t)', 'y(t)', 'FontSize',14);
 title ('Señal de control y salida del motor sin DC offset', 'FontSize',18);
 grid on;
 
 %% Abrir el system Identification
 systemIdentification();
+% Colocar en el 
 
-%% 
+%% Método de mínimos cuadrados
